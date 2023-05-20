190e39029956ac2411aa57d5a528c7189ee1ee4a
anasir1 <ayeshaacc99@gmail.com>
2023-05-20 12:26:07 +0500

    first commit

---
+++ table.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+# Create a connection
+conn = psycopg2.connect(
+    host="redshift-cluster.czmy70xfxylc.us-east-1.redshift.amazonaws.com",
+    port="5439",
+    dbname="dbdev",
+    user="useradmin",
+    password="Newpassword1"
+)
+
+# Create a cursor
+cursor = conn.cursor()
+
+# SQL statement to create a table
+create_table_query = """
+CREATE TABLE IF NOT EXISTS my_table (
+    id INT,
+    name VARCHAR(50)
+);
+"""
+
+# Execute the query
+cursor.execute(create_table_query)
+
+# Commit the transaction
+conn.commit()
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
